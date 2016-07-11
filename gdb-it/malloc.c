86dc8929da7fd8ba685d06e945192f1c5bfa43d4
leebaok <leebaok@163.com>
2016-07-11 21:05:45 +0800

    new gdb test

--- gdb-it/malloc.c
+++ gdb-it/malloc.c
@@ -4,7 +4,14 @@
 int main()
 {
 	void * p;
+	// tcache & small size
 	p = malloc(10);
+	// tcache & large size
+	p = malloc(20000);
+	// not tcache & large size 
+	p = malloc(1000000);
+	// not tcache & huge size
+	p = malloc(6000000);
 
 	return 0;
 }
